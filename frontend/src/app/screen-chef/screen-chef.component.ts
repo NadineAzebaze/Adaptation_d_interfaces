b29961e0e08b0cb45a7029d15cfe2e8ea7c3b7db
michael-marynowicz <michaelmarynowicz5@gmail.com>
2022-12-01 16:54:36 +0100

    suppression d'une table quand tout les plats ont été envoyés

--- frontend/src/app/screen-chef/screen-chef.component.ts
+++ frontend/src/app/screen-chef/screen-chef.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-screen-chef',
@@ -11,6 +11,7 @@ export class ScreenChefComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
 
 
 
--- frontend/src/app/table/table.component.ts
+++ frontend/src/app/table/table.component.ts
@@ -9,25 +9,37 @@ import TypeDish from "../models/typeDish.model";
 })
 
 export class TableComponent implements OnInit {
+  add =0;
 
   @Input() id! : number
+
 
   entree = new TypeDish("entree",["salade niçoise","salade de chèvre chaud"])
   plat = new TypeDish("plat",["daube","lasagne","escalope milanaise"])
   dessert = new TypeDish("dessert",["tiramisu","tropesiene"])
   table = new Table(this.id,[this.entree,this.plat,this.dessert])
 
-  constructor() { }
+  tableLength = this.entree.dishes.length + this.plat.dishes.length + this.dessert.dishes.length
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-
-
   strikeOut(dish: string) {
     // @ts-ignore
-    document.getElementById(dish).innerHTML = '<del>' + document.getElementById(dish).textContent + '</del>'
+    document.getElementById(dish).innerHTML = '<del>' + document.getElementById(dish).textContent + '</del>';
+    this.add+=1;
+    if(this.add===this.tableLength) this.deleteTable()
 
+  }
+
+  deleteTable(){
+    console.log("delete",this.id)
+
+    // @ts-ignore
+    document.getElementById("table"+this.id).remove();
   }
 }
 
