dcafc188bdb11056efa9128a0eaaf2a1e3c31e1d
YannClodong <yann.clodong@gmail.com>
2022-12-06 11:03:14 +0100

    :sparkles: Create selection page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ClerkPageComponent} from "./pages/clerk-module/clerk-page/clerk-page.component";
 import {ScreenChefComponent} from "./pages/screen-chef/screen-chef.component";
+import {RoleSelectionPageComponent} from "./pages/role-selection-page/role-selection-page.component";
 
 const routes: Routes = [
-  {path: 'clerk-page', component:ClerkPageComponent},
+  {path: 'clerk', component:ClerkPageComponent},
   {path: 'commands', component: ScreenChefComponent},
-  {path: '', component:ScreenChefComponent},
+  {path: '', component:RoleSelectionPageComponent},
 ];
 
 @NgModule({
