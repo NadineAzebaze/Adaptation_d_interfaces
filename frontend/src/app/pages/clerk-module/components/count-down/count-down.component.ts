11ada6f242c79df41159773ac665a5498dca0967
NadineAzebaze <53220838+NadineAzebaze@users.noreply.github.com>
2022-12-08 13:24:52 +0100

    Another test for timer not yet tested :\

---
+++ frontend/src/app/pages/clerk-module/components/count-down/count-down.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'app-count-down',
+  templateUrl: './count-down.component.html',
+  styleUrls: ['./count-down.component.css']
+})
+export class CountDownComponent implements OnInit, OnDestroy {
+
+    private subscription: Subscription;
+  
+    public dateNow = new Date();
+    public dDay = new Date('Jan 01 2021 00:00:00');
+    milliSecondsInASecond = 1000;
+    hoursInADay = 24;
+    minutesInAnHour = 60;
+    SecondsInAMinute  = 60;
+
+    public timeDifference;
+    public secondsToDday;
+    public minutesToDday;
+    public hoursToDday;
+    public daysToDday;
+
+
+    private getTimeDifference () {
+        this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+        this.allocateTimeUnits(this.timeDifference);
+    }
+
+  private allocateTimeUnits (timeDifference) {
+        this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+        this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+        this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+        this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+  }
+
+    ngOnInit() {
+       this.subscription = interval(1000)
+           .subscribe(x => { this.getTimeDifference(); });
+    }
+
+   ngOnDestroy() {
+      this.subscription.unsubscribe();
+   }
+
+}
